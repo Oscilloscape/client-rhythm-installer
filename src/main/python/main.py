dc3105c8713bfe5164af02c9353daf604f1b9513
clogic <software@oscilloscape.com>
2019-02-13 13:37:44 -0500

    separated mac and linux code and modified fel script to not require input

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -21,6 +21,7 @@ from shutil import copyfile
 from threading import Thread
 import shutil
 import json
+import shlex
 
 def is_linux():
     return platform == 'linux' or platform == 'linux2'
@@ -70,58 +71,84 @@ class InstallHandler(QThread):
     def initiate_fel_mode(self):
 
         os.makedirs(self.mount_dir, exist_ok=True)
-
-        # Catching fel-mass-storage failure
-
-        print(os.path.dirname(self.fel_mode_script))
-
-        p = subprocess.Popen([self.fel_mode_script], cwd=os.path.dirname(self.fel_mode_script), 
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         
-        while True:
-            if p.poll() is not None:
-                break
-            else:
-                out, err = p.communicate('\n'.encode('ascii'))
-                sys.stdout.write(str(out))
-                sys.stdout.flush()
-
+        out = subprocess.check_output([self.fel_mode_script],
+                                      cwd=os.path.dirname(self.fel_mode_script))
+        
+        out = out.decode()
+        print(out)
 
         return (True, 'Success')
 
     def mount(self):
-
-        fdisk_cmd = ['fdisk', '-l']
-        out = subprocess.check_output(fdisk_cmd)
-        out = out.decode()
-        lines = filter(lambda x : x.startswith('/dev'), out.split('\n'))
-        print(out)
-
+        
         self.partition = None
         self.system_partition = None
+        
+        if is_linux():
+            fdisk_cmd = ['fdisk', '-l']
+            out = subprocess.check_output(fdisk_cmd)
+            out = out.decode()
+            lines = filter(lambda x : x.startswith('/dev'), out.split('\n'))
+            print(out)
+        
+            for p in lines:
+                print(p.split())
+                part_info = p.split()
+                partition_name, boot_flag, size  = part_info[0], part_info[1], part_info[5]
+                if boot_flag == '*' and size == '255M':
+                    print('Mounting {0}'.format(partition_name))
+                    self.partition = partition_name
+                        
+        elif is_mac():
+            out = subprocess.check_output(shlex.split('diskutil list'))
+            out = out.decode()
+            diskutil_lines = out.split('\n')
+            for i, l in enumerate(diskutil_lines):
+                parts = list(filter(None, l.split(' ')))
+                print(parts)
+                if l.startswith('/dev') and parts[1].contains('external') and i + 4 < len(diskutil_lines):
+                    current_disk = l.split(' ')[0]
+                    print(current_disk)
+                    main_disk_line = diskutil_lines[i + 2]
+                    boot_part_line = diskutil_lines[i + 3]
+                    linux_part_line = diskutil_lines[i + 4]
+                    _, _, disk_size, _ = main_disk_line
+                    _, boot_name, boot_size, boot_id = boot_part_line
+                    _, linux_name, _, linux_id = linux_part_line
+                    print(disk_size, boot_name, boot_size, linux_name, linux_id)
 
-        for p in lines:
-            print(p.split())
-            part_info = p.split()
-            partition_name, boot_flag, size  = part_info[0], part_info[1], part_info[5]
-            if boot_flag == '*' and size == '255M':
-                print('Mounting {0}'.format(partition_name))
-                self.partition = partition_name 
+        else:
+            pass
+
+
 
         if self.partition:
-            p = subprocess.Popen(['mount', '-t', 'vfat' , self.partition, self.mount_dir], 
-                                 stdin=subprocess.PIPE)
-            if p.wait() != 0:
-                return (False, 'Failed to mount')
-            else: 
-                self.system_partition = self.partition[:-1] + str(int(self.partition[-1:]) + 1) 
+            if is_linux():
+                p = subprocess.Popen(['mount', '-t', 'vfat' , self.partition, self.mount_dir],
+                                     stdin=subprocess.PIPE)
+                if p.wait() != 0:
+                    return (False, 'Failed to mount')
+
+                self.system_partition = self.partition[:-1] + str(int(self.partition[-1:]) + 1)
                 print('Mounting {0} as SYSTEM'.format(self.system_partition))
                 p = subprocess.Popen(['udisksctl', 'mount', '--block-device', self.system_partition])
-
+                
                 if p.wait() != 0:
                     return (False, 'Failed to mount SYSTEM')
+
+            elif is_mac():
+                out = subprocess.check_output(shlex.split('diskutil mount {0}'.format(self.partition)))
+            
+                out = out.decode()
+                if not out.contains('mounted'):
+                    return (False, 'Failed to mount')
                 
-                print('Succeeded to mount SYSTEM')
+            else:
+                pass
+
+            
+            print('Succeeded to mount SYSTEM')
             
             return (True, 'Success')
 
@@ -129,44 +156,52 @@ class InstallHandler(QThread):
 
     def unmount(self):
         print('Unmounting {0} as SYSTEM'.format(self.system_partition))
+        
+        if is_linux():
 
-        p = subprocess.Popen(['udisksctl', 'unmount', '--block-device', self.system_partition])
-        if p.wait() != 0:
-            return (False, 'Failed to unmount SYSTEM')
+            p = subprocess.Popen(['udisksctl', 'unmount', '--block-device', self.system_partition])
+            if p.wait() != 0:
+                return (False, 'Failed to unmount SYSTEM')
 
-        p = subprocess.Popen(['umount', self.mount_dir], stdin=subprocess.PIPE)
-        p.communicate()
-        if p.wait() != 0:
-            return (False, 'Failed to unmount')
+            p = subprocess.Popen(['umount', self.mount_dir], stdin=subprocess.PIPE)
+            p.communicate()
+            if p.wait() != 0:
+                return (False, 'Failed to unmount')
+
+        elif is_mac():
+            out = subprocess.check_output(shlex.split('diskutil unmount {0}'.format()))
+
+        else:
+            pass
         
         self.completed_before = True
         return (True, 'Finished with unmount')
 
     def download_files(self):
-        os.makedirs(self.copy_directory, exist_ok=True)
+        #os.makedirs(self.copy_directory, exist_ok=True)
 
         s3_bucket_url = self.config.s3_bucket_url
 
-        try:
-            self.progress_signal.emit('Downloading latest updates...', 0, 100)
+        # try:
+        self.progress_signal.emit('Downloading latest updates...', 0, 100)
 
-            r = requests.get('{0}/latest-version'.format(s3_bucket_url))
-            if r.status_code != 200:
-                return (False, 'Failed to get latest updates')
-            latest_version = r.content.strip().decode()
+        r = requests.get('{0}/latest-version'.format(s3_bucket_url))
+        if r.status_code != 200:
+            return (False, 'Failed to get latest updates')
+        latest_version = r.content.strip().decode()
 
-            for file_name, dest in self.files_dest.items():
-                file_url = '{0}/{1}/{2}'.format(s3_bucket_url, latest_version, file_name)
-                r = requests.get(file_url)
-                if r.status_code == 200:
-                    file_copy_dest = '{0}/{1}'.format(self.copy_directory, file_name)
-                    with open(file_copy_dest, 'wb') as f:
-                        f.write(r.content)
-                    print('Downloaded {0}'.format(file_name))
-                else:
-                    print('Did not find or failed to download {0}'.format(file_url))
-        except Exception as e:
-            return (False, str(e))
+        for file_name, dest in self.files_dest.items():
+            file_url = '{0}/{1}/{2}'.format(s3_bucket_url, latest_version, file_name)
+            r = requests.get(file_url)
+            if r.status_code == 200:
+                file_copy_dest = '{0}/{1}'.format(self.copy_directory, file_name)
+                with open(file_copy_dest, 'wb') as f:
+                    f.write(r.content)
+                self.progress_signal.emit('Downloaded {0}'.format(file_name), 0, 100)
+            else:
+                print('Did not find or failed to download {0}'.format(file_url))
+        # except Exception as e:
+        #    return (False, str(e))
 
         return (True, '')
 
@@ -195,16 +230,6 @@ class InstallHandler(QThread):
             except Exception as e:
                 return (False, 'Copy failed from {0} to {1}: {2}'.format(file_name, dest, e))
 
-            # p = subprocess.Popen(['cp', file_name, dest], 
-            #         stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            # out, err = p.communicate() # '{0}\n'.format(self.sudo_password))
-            # if out:
-            #     print(str(out))
-            # if err:
-            #     print(err)
-            # if p.wait() != 0:
-            #     print()
-            #     return -1
             return (True, '')
 
         for f, dest in self.files_dest.items():
@@ -231,8 +256,11 @@ class InstallHandler(QThread):
         if self.completed_before:
             self.unplug_signal.emit()
         else:
-            success, msg = self.download_files()    
+            pass
+            # success, msg = self.download_files()
 
+        success = True
+        
         if is_linux() or is_mac():
             if success:
                 success, msg = self.initiate_fel_mode()
@@ -283,6 +311,11 @@ class AppContext(ApplicationContext):
         self.progress_bar = QProgressBar()
         self.progress_bar.setMaximum(100)
         self.install_handler.progress_signal.connect(self.update_progress)
+        
+        if is_mac():
+            self.progress_status = QStatusBar()
+            self.install_handler.progress_signal.connect(self.update_progress_text)
+            window.setStatusBar(self.progress_status)
 
         self.install_handler.unplug_signal.connect(self.prompt_unplug)
 
@@ -318,6 +351,9 @@ class AppContext(ApplicationContext):
     def update_progress(self, current, done, total):
         self.progress_bar.setValue((done / total) * 100)
         self.progress_bar.setFormat('{0}'.format(current))
+    
+    def update_progress_text(self, current, done, total):
+        self.progress_status.showMessage(current)
 
     def prompt_unplug(self):
         prompt = QMessageBox()
