eaaaa71595582f27cd9316ab886e5ca602721de5
Jon Forsyth <jon.forsyth@oscilloscape.com>
2019-06-12 17:40:33 -0400

    cd to correct directory. If on Mac OS, export DYLIB path. Put quotes around directory paths in case of spaces.

--- src/main/resources/base/fel-mass-storage/start.sh
+++ src/main/resources/base/fel-mass-storage/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd "$FEL_DIR"
+
 BOARD=h3
 
-$SUNXI_FEL -p uboot $BOARD/u-boot-sunxi-with-spl.bin write 0x42000000 $BOARD/zImage write 0x43000000 $BOARD/script.bin write 0x43300000 $BOARD/uInitrd write 0x43100000 $BOARD/boot.scr
+OSNAME="$(uname -s)"
+if [[ "$OSNAME" == "Darwin"* ]]; then
+	export DYLD_LIBRARY_PATH="$LIB_PATH":$DYLD_LIBRARY_PATH
+fi
+
+"$SUNXI_FEL" -p uboot $BOARD/u-boot-sunxi-with-spl.bin write 0x42000000 $BOARD/zImage write 0x43000000 $BOARD/script.bin write 0x43300000 $BOARD/uInitrd write 0x43100000 $BOARD/boot.scr
